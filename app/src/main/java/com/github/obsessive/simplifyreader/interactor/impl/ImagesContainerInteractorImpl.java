691722ce1fb997b133fe67eba8c50f8d39bda3eb
Xing Li <sam.lxing@gmail.com>
2015-09-18 14:19:13 +0800

    Add supports to english.

--- app/src/main/java/com/github/obsessive/simplifyreader/interactor/impl/ImagesContainerInteractorImpl.java
+++ app/src/main/java/com/github/obsessive/simplifyreader/interactor/impl/ImagesContainerInteractorImpl.java
@@ -35,13 +35,14 @@ public class ImagesContainerInteractorImpl implements CommonContainerInteractor 
     @Override
     public List<BaseEntity> getCommonCategoryList(Context context) {
         List<BaseEntity> resultData = new ArrayList<>();
-        String[] imagesCategoryArray = context.getResources().getStringArray(R.array.images_category_list);
-        resultData.add(new BaseEntity(imagesCategoryArray[0], imagesCategoryArray[0]));
-        resultData.add(new BaseEntity(imagesCategoryArray[1], imagesCategoryArray[1]));
-        resultData.add(new BaseEntity(imagesCategoryArray[2], imagesCategoryArray[2]));
-        resultData.add(new BaseEntity(imagesCategoryArray[3], imagesCategoryArray[3]));
-        resultData.add(new BaseEntity(imagesCategoryArray[4], imagesCategoryArray[4]));
-        resultData.add(new BaseEntity(imagesCategoryArray[5], imagesCategoryArray[5]));
+        String[] imagesCategoryArrayId = context.getResources().getStringArray(R.array.images_category_list_id);
+        String[] imagesCategoryArrayName = context.getResources().getStringArray(R.array.images_category_list_name);
+        resultData.add(new BaseEntity(imagesCategoryArrayId[0], imagesCategoryArrayName[0]));
+        resultData.add(new BaseEntity(imagesCategoryArrayId[1], imagesCategoryArrayName[1]));
+        resultData.add(new BaseEntity(imagesCategoryArrayId[2], imagesCategoryArrayName[2]));
+        resultData.add(new BaseEntity(imagesCategoryArrayId[3], imagesCategoryArrayName[3]));
+        resultData.add(new BaseEntity(imagesCategoryArrayId[4], imagesCategoryArrayName[4]));
+        resultData.add(new BaseEntity(imagesCategoryArrayId[5], imagesCategoryArrayName[5]));
         return resultData;
     }
 }
